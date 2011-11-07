3c3956888798f329ba04ff711275a490dcfa95e8
charlesmarshall <cm56marshall@googlemail.com>
2011-11-07 12:10:36 +0000

    fixes and changes to the calendar controller

--- lib/controller/WildfireEventsController.php
+++ lib/controller/WildfireEventsController.php
@@ -1,6 +1,7 @@
 <?
 class WildfireEventsController extends ApplicationController {
   public $cms_live_scope = "event";
+  public $cms_list_scope = "event_list";
   public $per_page = 3;
   public $months_events = array();
 
@@ -8,26 +9,27 @@ class WildfireEventsController extends ApplicationController {
     WaxEvent::add("cms.cms_stack_set", function(){
       $obj = WaxEvent::data();
       array_unshift($obj->cms_stack, $this->controller);
-    });    
+    });
     parent::controller_global();
   }
-  
+
   public function __calendar(){
     if(!$cal_month = Request::get('month')) $cal_month = date("m");
     elseif($cal_month < 10) $cal_month = "0".str_replace("0", "", $cal_month);
     if(!$cal_year = Request::get('year')) $cal_year = date("Y");
-    $model = new $this->cms_content_class();
-    $model = $model->scope($this->cms_live_scope);
+    $model = new $this->cms_content_class($this->cms_list_scope);
+    $model = $model->scope();
     $cal = new Calendar();
-    
+
     if(($events = $cal->event_range_filter($model, $cal_year, $cal_month)->all()) && $events->count()){
+
       foreach($events as $event){
         $index = date("Y-n-j", strtotime($event->event_date_start));
         $this->months_events[$index][$event->primval] = $event;
-        if($event->event_date_end > $event->event_date_start){
-          $min = date("Ymd", strtotime($event->event_date_start));
-          $max = date("Ymd", strtotime($event->event_date_end));
-          foreach(range($min, $max) as $i){
+        $start = date("Ymd", strtotime($event->event_date_start));
+        $end = date("Ymd", strtotime($event->event_date_end));
+        if($end > $start){
+          foreach(range($start, $end) as $i){
             $ind = date("Y-n-j", strtotime($i));
             $this->months_events[$ind][$event->primval] = $event;
           }
@@ -37,5 +39,4 @@ class WildfireEventsController extends ApplicationController {
   }
 
 }
-?>
 ?>
