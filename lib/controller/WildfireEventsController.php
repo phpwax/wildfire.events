fb86da5f7edf2a29c4ab94b2b8978bfc705ec2bb
Mike <michal.noskovic@googlemail.com>
2012-11-14 13:30:21 +0000

    the scope needs to be scope(scope_name)

--- lib/controller/WildfireEventsController.php
+++ lib/controller/WildfireEventsController.php
@@ -17,8 +17,8 @@ class WildfireEventsController extends ApplicationController {
     if(!$cal_month = Request::get('month')) $cal_month = date("m");
     elseif($cal_month < 10) $cal_month = "0".str_replace("0", "", $cal_month);
     if(!$cal_year = Request::get('year')) $cal_year = date("Y");
-    $model = new $this->cms_content_class($this->cms_list_scope);
-    $model = $model->scope();
+    $model = new $this->cms_content_class;
+    $model = $model->scope($this->cms_list_scope);
     $cal = new Calendar();
 
     if(($events = $cal->event_range_filter($model, $cal_year, $cal_month)->all()) && $events->count()){
