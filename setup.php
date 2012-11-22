090feee093495464b1557df4d4074302def8c2dd
charles marshall <charles@oneblackbear.com>
2012-11-22 14:12:06 +0000

    add hours & minutes

--- setup.php
+++ setup.php
@@ -9,8 +9,8 @@ if(!defined("CONTENT_MODEL")){
 WaxEvent::add(CONTENT_MODEL.".setup", function(){
   $obj = WaxEvent::data();
   $obj->define("event_item", "BooleanField", array('group'=>'event', 'label'=>'Is an event'));
-  $obj->define("event_date_start", "DateTimeField", array('group'=>'event', 'label'=>'Event start','output_format'=>"j F Y",'input_format'=>"j F Y H:i", 'default'=>date("Y-m-d") ));
-  $obj->define("event_date_end", "DateTimeField", array('group'=>'event', 'label'=>'Event end','output_format'=>"j F Y",'input_format'=>"j F Y H:i", 'default'=>date("Y-m-d",mktime(0,0,0, date("m"), date("j")+10, date("y") )) ));
+  $obj->define("event_date_start", "DateTimeField", array('group'=>'event', 'label'=>'Event start','output_format'=>"j F Y H:i",'input_format'=>"j F Y H:i", 'default'=>date("Y-m-d") ));
+  $obj->define("event_date_end", "DateTimeField", array('group'=>'event', 'label'=>'Event end','output_format'=>"j F Y H:i",'input_format'=>"j F Y H:i", 'default'=>date("Y-m-d",mktime(0,0,0, date("m"), date("j")+10, date("y") )) ));
   $obj->define("event_address", "TextField", array('group'=>'event'));
   $obj->define("event_postcode", "CharField", array('group'=>'event'));    
   $obj->define("event_featured", "BooleanField", array('group'=>'event', 'label'=>'Featured event?'));
