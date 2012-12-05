b760ca29786b0f4e04da450085b7d9562a9fdb54
charles marshall <charles@oneblackbear.com>
2012-12-05 11:34:09 +0000

    fix scope, not $this but the $obj

--- lib/controller/WildfireEventsController.php
+++ lib/controller/WildfireEventsController.php
@@ -8,7 +8,7 @@ class WildfireEventsController extends ApplicationController {
   public function controller_global(){
     WaxEvent::add("cms.cms_stack_set", function(){
       $obj = WaxEvent::data();
-      array_unshift($obj->cms_stack, $this->controller);
+      array_unshift($obj->cms_stack, $obj->controller);
     });
     parent::controller_global();
   }
